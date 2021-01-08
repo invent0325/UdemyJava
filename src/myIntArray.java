bd466cc66ff02ecf972dd8087473c1a82cac4e4d
Lawrence <lpinto1204@gmail.com>
2021-01-08 15:27:37 +0530

    All Commits done

--- src/myIntArray.java
+++ src/myIntArray.java
@@ -4,9 +4,15 @@ public class myIntArray {
         int[] myIntArrayValue = new int[10];
         int[] myIntAnotheArray = {1, 2, 3, 4, 5};
         double[] myDoubleArrayValue = new double[10];
+        int[] myForArrayLoop = new int[10];
 
         myIntArrayValue[5] = 50;
+        for(int i = 0; i < 10; i++) {
+            myForArrayLoop[i] = i * 10;
+        }
+
         System.out.println(myIntArrayValue[5]);
         System.out.println(myIntAnotheArray[1]);
+        System.out.println(myForArrayLoop[2]);
     }
 }
