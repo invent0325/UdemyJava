ec57ba082df5dfbf79eb87bd0c494abd89be0f8c
Lawrence <lpinto1204@gmail.com>
2021-01-08 15:24:04 +0530

    All Commits done

--- src/myIntArray.java
+++ src/myIntArray.java
@@ -2,9 +2,11 @@ public class myIntArray {
     public static void main(String[] args) {
 
         int[] myIntArrayValue = new int[10];
+        int[] myIntAnotheArray = {1, 2, 3, 4, 5};
         double[] myDoubleArrayValue = new double[10];
 
         myIntArrayValue[5] = 50;
         System.out.println(myIntArrayValue[5]);
+        System.out.println(myIntAnotheArray[1]);
     }
 }
