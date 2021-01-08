75099854365d484b44d602cdbe324bcca1ce9585
Lawrence <lpinto1204@gmail.com>
2021-01-08 15:20:56 +0530

    All Commits done

---
+++ src/myIntArray.java
@@ -0,0 +1,10 @@
+public class myIntArray {
+    public static void main(String[] args) {
+
+        int[] myIntArrayValue = new int[10];
+        double[] myDoubleArrayValue = new double[10];
+
+        myIntArrayValue[5] = 50;
+        System.out.println(myIntArrayValue[5]);
+    }
+}
